13cd5730a44d014b69bc89c702422b9e8cee61cc
xybayue-tbk <xybayue@gmail.com>
2026-04-16 15:09:01 +0700

    isi looping

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -7,6 +7,12 @@ int x;
 
 void input(){
     while (true){
-        
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10) : ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+            break;
+        else
+            cout << "\nArray dapat mempunyai maksimal 10 elemen.\n";
     }
 }
