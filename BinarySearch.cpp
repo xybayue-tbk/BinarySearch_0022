e745e31468a9a678d0fc5a0fa42eef38dd75625f
xybayue-tbk <xybayue@gmail.com>
2026-04-16 15:21:53 +0700

    bubbleSortArray

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -28,5 +28,18 @@ cout << "-------------------\n";
 }
 
 void bubbleSortArray(){
-    
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang -1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }  
+        pass++;
+    } while (pass < nPanjang - 1);
 }
