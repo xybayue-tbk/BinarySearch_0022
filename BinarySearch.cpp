160b45e0f086a3e0b7ab110f1ec9228ec324832f
xybayue-tbk <xybayue@gmail.com>
2026-04-16 15:25:37 +0700

    progam display

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -45,8 +45,18 @@ void bubbleSortArray(){
 }
 
 void display(){
-    
+
     cout << "\n===============================\n";
     cout << " Elemen Array Setelah Diurutkan (Asc)\n";
     cout << "===============================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
